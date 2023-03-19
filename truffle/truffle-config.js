1c57e6dc9405b5aa343beb937c7c03f97dc2fc9f
fabcoulon <fabiencoulon1@yahoo.fr>
2023-03-19 23:35:59 +0100

    put in production

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -14,6 +14,10 @@ module.exports = {
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    goerli: {
+      provider: function(){return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://goerli.infura.io/v3/${process.env.INFURA_ID}`})},
+      network_id:5,
+      },
     mumbai: {
       provider: function(){return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`})},
       network_id:80001,
